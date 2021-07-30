6f3a206d34b7c6b8c87788d789cf9bc1850a1159
Miguel <kckeiks+github@gmail.com>
2021-07-30 18:05:32 -0400

    init fixed header struct

--- lib/packet.go
+++ lib/packet.go
@@ -1,10 +1,9 @@
 package lib
 
 import (
+    "errors"
     "io"
-	"fmt"
-	"encoding/hex"
-
+    "fmt"
 )
 
 const (
@@ -12,10 +11,32 @@ const (
     Connack = 2
 )
 
+const (
+    HeaderFlagMask = 0x0F
+)
+
 type FixedHeader struct {
     pktType byte
     flags byte
-    remLength byte
+    remLength int
+}
+
+func GetRemLength(r io.Reader) (int, error) {
+    // http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Table_2.4_Size    
+    mul := 1
+    val := 0
+    encodedByte := make([]byte, 1)
+    for {
+        r.Read(encodedByte)
+        val += int(encodedByte[0] & byte(127)) * mul
+        mul *= 128
+        if (mul > 128*128*128) {
+            return 0, errors.New("Error: Invalid remaining length.")
+        }
+        if ((encodedByte[0] & byte(128)) == 0) {
+            return val, nil
+        }
+    }
 }
 
 func GetControlPkt(r io.Reader) *ConnectPkt {
@@ -24,13 +45,20 @@ func GetControlPkt(r io.Reader) *ConnectPkt {
     if err != nil {
         return nil
     }
-    fh := FixedHeader{pktType: buff[0]}
-	pt := fh.pktType >> 4
-	fmt.Printf("Hex Dump:%s", hex.Dump([]byte{pt}))
-	if pt == Connect {
+
+    flags := buff[0] & HeaderFlagMask 
+    pktType := buff[0] >> 4
+    rl, lenErr := GetRemLength(r)
+    if lenErr != nil {
+        return nil
+    }
+    fh := FixedHeader{pktType: pktType, flags: flags, remLength: rl}
+
+	if pktType == Connect {
         cp := &ConnectPkt{fixedHeader: fh}
-		fmt.Printf("ConnectPkt: %+v\n", cp)
+        fmt.Printf("ConnectPkt: %+v\n", cp)
 		return cp
     }
 	return nil
 }
+
