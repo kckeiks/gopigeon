05225bb9780f1569643132c07011cf7c0b9f7030
Miguel <kckeiks+github@gmail.com>
2021-08-02 21:44:28 -0400

    get and validate procotol name

--- lib/connect.go
+++ lib/connect.go
@@ -4,7 +4,11 @@ import (
     "bytes"
     "encoding/binary"
     "fmt"
+    "strings"
+    "unicode/utf8"
 )
+
+const ProtocolName = "MQTT"
 
 type ConnectPkt struct {
     pktType byte 
@@ -26,9 +30,21 @@ func (cp *ConnectPkt) decode(flags byte, packet []byte) error {
     if (n != 2 || err != nil) {
         return nil
     }
-    fmt.Println(binary.BigEndian.Uint16(protocolNameLen))
-    if (binary.BigEndian.Uint16(protocolNameLen) != 4) {
+    pnl := binary.BigEndian.Uint16(protocolNameLen)
+    fmt.Println(pnl)
+    if (pnl != 4) {
         return nil
     }
+    protocol := make([]byte, pnl)
+    n, err = buff.Read(protocol)
+    if (n != int(pnl) || err != nil) {
+        return nil
+    }
+    p := string(protocol)
+    // TODO: Read about strings and UTF-8
+    if (!utf8.ValidString(p) || strings.Compare(p, ProtocolName) != 0) {
+        return nil
+    }
+    fmt.Println(p)
     return nil
 }
