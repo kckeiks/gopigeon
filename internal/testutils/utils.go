6e0027b434f765a043955050ad68442e9ff31402
Miguel <kckeiks+github@gmail.com>
2021-09-15 16:53:04 -0400

    add connect tests

--- internal/testutils/utils.go
+++ internal/testutils/utils.go
@@ -3,23 +3,15 @@ package testutils
 import (
 	"bytes"
 	"io"
-	"github.com/kckeiks/gopigeon/lib"
 )
 
-func NewTestFixedHeader() io.ReadWriter {
+func NewTestEncodedFixedHeader() io.ReadWriter {
 	return bytes.NewBuffer([]byte{16, 12, 0, 4})
 }
 
 // TODO: Maybe refactor this so it looks better
 func NewTestEncodedConnectPkt() []byte {
 	return []byte{16, 12, 0, 4, 77, 81, 84, 84, 4, 2, 0, 60, 0, 0}
-}
-
-func NewTestDecodedConnackPkt() ConnackPacket {
-	return lib.ConnackPacket{
-        AckFlags: 0,
-        RtrnCode: 0,
-    }
 }
 
 func NewTestReadWriterConnect() io.ReadWriter {
