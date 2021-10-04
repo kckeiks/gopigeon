1e75ccb80aeb07ea8037e4977c882432b0c59d8f
Miguel <kckeiks+github@gmail.com>
2021-10-03 23:01:02 -0400

    rename to test util

--- gopigeon_test.go
+++ gopigeon_test.go
@@ -9,37 +9,37 @@ import (
 )
 
 // implements net.Conn interface
-type testClient struct {
+type testConn struct {
 	b bytes.Buffer
 }
 
-func (tc *testClient) WritePacket(b []byte) {
+func (tc *testConn) WritePacket(b []byte) {
 	tc.b.Write(b)  
 }
 
-func (tc *testClient) Read(b []byte) (int, error) { 
+func (tc *testConn) Read(b []byte) (int, error) { 
 	return tc.b.Read(b)  
 }
 
-func (tc *testClient) Write(b []byte) (int, error) {
+func (tc *testConn) Write(b []byte) (int, error) {
 	// We are not interested in the *ack messages
 	return len(b), nil
 }
 
-func (*testClient) Close() error { return nil }
-func (*testClient) LocalAddr() net.Addr { return nil }
-func (*testClient) RemoteAddr() net.Addr { return nil }
-func (*testClient) SetDeadline(t time.Time) error { return nil }
-func (*testClient) SetReadDeadline(t time.Time) error { return nil }
-func (*testClient) SetWriteDeadline(t time.Time) error { return nil }
+func (*testConn) Close() error { return nil }
+func (*testConn) LocalAddr() net.Addr { return nil }
+func (*testConn) RemoteAddr() net.Addr { return nil }
+func (*testConn) SetDeadline(t time.Time) error { return nil }
+func (*testConn) SetReadDeadline(t time.Time) error { return nil }
+func (*testConn) SetWriteDeadline(t time.Time) error { return nil }
 
 func TestHandleConnTwoConnects(t *testing.T) {
-	client := &testClient{}
+	c := &testConn{}
 	_, cp1 := newTestEncodedConnectPkt()
 	_, cp2 := newTestEncodedConnectPkt()
-	client.WritePacket(cp1)
-	client.WritePacket(cp2)
-	err := HandleConn(client)
+	c.WritePacket(cp1)
+	c.WritePacket(cp2)
+	err := HandleConn(c)
 	if err != SecondConnectPktError {
 		t.Fatalf("Expected SecondConnectPktError but got %d.", err)
 	}
